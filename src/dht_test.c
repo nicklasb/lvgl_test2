69afb6c2ae76e4b7d945589feeb5e8951d145aca
Nicklas Börjesson <nicklas@optimalbpm.se>
2022-03-27 12:34:51 +0200

    Fix DHT compilation

---
+++ src/dht_test.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include "esp_system.h"
+#include "freertos/event_groups.h"
+#include "driver/gpio.h"
+#include "esp_event.h"
+#include "nvs_flash.h"
+#include "esp_log.h"
+
+
+#include "DHT22-lib-for-esp-idf/include/DHT.h"
+
+static const char *TAG = "DHT";
+
+void DHT_task(void *pvParameter)
+{
+    setDHTgpio(GPIO_NUM_4);
+    ESP_LOGI(TAG, "Starting DHT Task\n\n");
+
+    while (1)
+    {
+        ESP_LOGI(TAG, "=== Reading DHT ===\n");
+        int ret = readDHT();
+
+        errorHandler(ret);
+
+        ESP_LOGI(TAG, "Hum: %.1f Tmp: %.1f\n", getHumidity(), getTemperature());
+
+        // -- wait at least 2 sec before reading again ------------
+        // The interval of whole process must be beyond 2 seconds !!
+        vTaskDelay(2000 / portTICK_RATE_MS);
+    }
+}
+
+void init_DHT()
+{
+    //Initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES)
+    {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    esp_log_level_set("*", ESP_LOG_INFO);
+
+    xTaskCreatePinnedToCore(&DHT_task, "DHT_task", 2048, NULL, 5, NULL, 1);
+}
